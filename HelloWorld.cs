9fc7734f45f11e9de6c88f6dc72f9ad205b0b7bd
chodie <dennisterrell@gmail.com>
2014-05-07 07:49:29 -0700

    Initial commit of Helloworld in C#

---
+++ HelloWorld.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class HelloWord
+    {
+        static void Main(string[]args)
+        {
+            System.Console.WriteLine("Hello. My name is Dennis and I suck at this so far.");
+            Console.WriteLine("CommandLine: {0}", Environment.CommandLine);
+            Console.Read();
+
+        }
+    }
+}
